c55137815827353c83209efee2eef7e72c9643d6
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-02-19 02:25:19 +0500

    create DataBase

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+const faunaDB=require("faunadb"),
+q=faunaDB.query;
+
+
+(async ()=>{
+
+var adminClient = new faunaDB.Client({ secret: "fnAECXwUqYACASM7mVf1cWGOIwauTZHaRy9QTwTi" });
+
+            // createDaataBase
+
+//         try{
+//             let result= await adminClient.query(
+//                      q.CreateDatabase({name:"gatsbyDataBase"})
+//             );
+//             console.log(result);
+//         }
+//         catch(error){
+//    console.log("Error",error);
+//         }
+
+})()
